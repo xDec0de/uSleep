13018221010fb468a2e323ccc7446b24398918f3
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-27 19:49:23 +0100

    Cleaned SuperVanish method

--- src/es/xdec0de/usleep/utils/SuperVanish.java
+++ src/es/xdec0de/usleep/utils/SuperVanish.java
@@ -4,12 +4,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.metadata.MetadataValue;
 
 public class SuperVanish {
+
 	public static boolean isVanished(Player player) {
-	    for(MetadataValue meta : player.getMetadata("vanished")) {
-	    	if(meta.asBoolean()) {
-	    		return true;
-	    	}
-	    } 
-	    return false;
+		for(MetadataValue meta : player.getMetadata("vanished"))
+			if(meta.asBoolean())
+				return true;
+		return false;
 	}
 }

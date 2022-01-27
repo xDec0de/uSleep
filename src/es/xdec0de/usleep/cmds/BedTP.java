515819cdfa7180e7f060ae1dd1186832471770cd
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-27 18:29:04 +0100

    /bedtp optimization

--- src/es/xdec0de/usleep/cmds/BedTP.java
+++ src/es/xdec0de/usleep/cmds/BedTP.java
@@ -1,61 +1,56 @@
 package es.xdec0de.usleep.cmds;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 import es.xdec0de.usleep.utils.USPMessage;
 import es.xdec0de.usleep.utils.USPSetting;
 import es.xdec0de.usleep.utils.files.USPConfig;
 import es.xdec0de.usleep.utils.files.USPMessages;
 
 public class BedTP implements CommandExecutor {
-	
+
 	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
 		if(sndr instanceof Player) {
 			Player p = (Player)sndr;
 			if(args.length == 0) {
 				if(p.hasPermission(USPConfig.getString(USPSetting.BEDTP_PERM))) {
-					if(((Player) sndr).getBedLocation() != null) {
-						p.teleport(p.getBedSpawnLocation());
-					} else {
+					Location bed = p.getBedSpawnLocation();
+					if(bed != null)
+						p.teleport(bed);
+					else
 						USPMessages.sendMessage(sndr, USPMessage.BEDTP_ERROR);
-					}
-				} else {
-					sndr.sendMessage(USPMessages.getMessage(USPMessage.NO_PERMS).replaceAll("%perm%", USPConfig.getString(USPSetting.BEDTP_PERM)));
-				}
+				} else
+					USPMessages.sendMessage(sndr, USPMessage.NO_PERMS, "%perm%", USPConfig.getString(USPSetting.BEDTP_PERM));
 			} else if(args.length == 1) {
 				if(sndr.hasPermission(USPConfig.getString(USPSetting.BEDTP_OTHER_PERM))) {
-					if(getBedLocation(args[0]) != null) {
-						((Player) sndr).teleport(getBedLocation(args[0]));
-						if(Bukkit.getPlayer(args[0]) != null) {
-							sndr.sendMessage(USPMessages.getMessage(USPMessage.BEDTP_TELEPORT_OTHER).replaceAll("%player%", Bukkit.getPlayer(args[0]).getName()));
-						} else {
-							sndr.sendMessage(USPMessages.getMessage(USPMessage.BEDTP_TELEPORT_OTHER).replaceAll("%player%", args[0]));
-						}
-					} else {
+					Location bed = getBedLocation(args[0]);
+					if(bed != null) {
+						p.teleport(bed);
+						USPMessages.sendMessage(sndr, USPMessage.BEDTP_TELEPORT_OTHER, "%player%", args[0]);
+					} else
 						USPMessages.sendMessage(sndr, USPMessage.BEDTP_ERROR);
-					}
-				} else {
-					sndr.sendMessage(USPMessages.getMessage(USPMessage.NO_PERMS).replaceAll("%perm%", USPConfig.getString(USPSetting.BEDTP_OTHER_PERM)));
-				}
-			} else {
+				} else
+					USPMessages.getMessage(USPMessage.NO_PERMS, "%perm%", USPConfig.getString(USPSetting.BEDTP_OTHER_PERM));
+			} else
 				USPMessages.sendMessage(sndr, USPMessage.BEDTP_USAGE);
-			}
-		} else {
+		} else
 			USPMessages.sendMessage(sndr, USPMessage.NO_CONSOLE);
-		}
 		return true;
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	private Location getBedLocation(String player) {
-		if(Bukkit.getPlayer(player) != null) {
-			return Bukkit.getPlayer(player).getBedSpawnLocation();
-		} else if(Bukkit.getOfflinePlayer(player) != null) {
-			return Bukkit.getOfflinePlayer(player).getBedSpawnLocation();
-		}
+		Player target = Bukkit.getPlayerExact(player);
+		if(target != null)
+			return target.getBedSpawnLocation();
+		OfflinePlayer offlineTarget = Bukkit.getOfflinePlayer(player);
+		if(offlineTarget != null)
+			return offlineTarget.getBedSpawnLocation();
 		return null;
 	}
 }

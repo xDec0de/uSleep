d4d5f1b6ceb1c33e5c3961492052aff25835e1ec
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-27 20:01:49 +0100

    Cleaned startup code

--- src/es/xdec0de/usleep/USleep.java
+++ src/es/xdec0de/usleep/USleep.java
@@ -1,7 +1,6 @@
 package es.xdec0de.usleep;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import es.xdec0de.usleep.cmds.BedTP;
@@ -16,107 +15,96 @@ public class USleep  extends JavaPlugin {
 	private static USleep instance;
 
 	public void onEnable() {
-		instance = this;
-		setupFiles();
-		registerCommands();
-		registerEvents();
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "             &e&luSleep &8- &aEnabled"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &b- &7Author&8: &bxDec0de_"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &b- &7Version: &b"+getDescription().getVersion()));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-		Bukkit.getConsoleSender().sendMessage(" ");
+		executeEnable();
+		USPMessages.log(" ");
+		USPMessages.logCol("&8|------------------------------------------>");
+		USPMessages.log(" ");
+		USPMessages.logCol("             &e&luSleep &8- &aEnabled");
+		USPMessages.log(" ");
+		USPMessages.logCol("  &b- &7Author&8: &bxDec0de_");
+		USPMessages.log(" ");
+		USPMessages.logCol("  &b- &7Version: &b"+getDescription().getVersion());
+		USPMessages.log(" ");
+		USPMessages.logCol("&8|------------------------------------------>");
+		USPMessages.log(" ");
 		checkDependencies();
 		checkUpdates();
 	}
 
 	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "             &e&luSleep &8- &cDisabled"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &b- &7Author&8: &bxDec0de_"));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &b- &7Version: &b"+getDescription().getVersion()));
-		Bukkit.getConsoleSender().sendMessage(" ");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-		Bukkit.getConsoleSender().sendMessage(" ");
+		USPMessages.log(" ");
+		USPMessages.logCol("&8|------------------------------------------>");
+		USPMessages.log(" ");
+		USPMessages.logCol("             &e&luSleep &8- &cDisabled");
+		USPMessages.log(" ");
+		USPMessages.logCol("  &b- &7Author&8: &bxDec0de_");
+		USPMessages.log(" ");
+		USPMessages.logCol("  &b- &7Version: &b"+getDescription().getVersion());
+		USPMessages.log(" ");
+		USPMessages.logCol("&8|------------------------------------------>");
+		USPMessages.log(" ");
 	}
 
-	private void setupFiles() {
+	private void executeEnable() {
+		instance = this;
 		USPConfig.setup();
 		USPConfig.save(); // TODO What?
 		USPMessages.setup();
-	}
-
-	private void registerCommands() {
 		getCommand("usleep").setExecutor(new USleepCMD());
 		getCommand("bedtp").setExecutor(new BedTP());
-	}
-
-	private void registerEvents() {
-		this.getServer().getPluginManager().registerEvents(new SleepHandler(), this);
-		this.getServer().getPluginManager().registerEvents(new UpdateChecker(), this);
+		getServer().getPluginManager().registerEvents(new SleepHandler(), this);
+		getServer().getPluginManager().registerEvents(new UpdateChecker(), this);
 	}
 
 	private void checkDependencies() {
-		if(Bukkit.getPluginManager().getPlugin("Essentials") != null) {
-			Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &e- &bEssentials &7detected (&av" + Bukkit.getPluginManager().getPlugin("Essentials").getDescription().getVersion() + "&7)"));
-			Bukkit.getConsoleSender().sendMessage(" ");
-		} else {
-			Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &e- &eEssentials &cnot detected, disabling &6AFK &cand &6Vanish &ccheck. &8(&bEssentials only&8)"));
-			Bukkit.getConsoleSender().sendMessage(" ");
-			USPConfig.get().set("Essentials.IgnoreAFK", Boolean.valueOf(false));
-			USPConfig.get().set("Essentials.IgnoreVanished", Boolean.valueOf(false));
+		if(Bukkit.getPluginManager().getPlugin("Essentials") != null)
+			USPMessages.logCol("  &e- &bEssentials &7detected (&av" + Bukkit.getPluginManager().getPlugin("Essentials").getDescription().getVersion() + "&7)");
+		else {
+			USPMessages.logCol("  &e- &eEssentials &cnot detected, disabling &6AFK &cand &6Vanish &ccheck. &8(&bEssentials only&8)");
+			USPConfig.get().set("Essentials.IgnoreAFK", false);
+			USPConfig.get().set("Essentials.IgnoreVanished", false);
 			USPConfig.save();
 			USPConfig.reload();
 		}
+		USPMessages.log(" ");
 		if(Bukkit.getPluginManager().getPlugin("SuperVanish") != null || Bukkit.getPluginManager().getPlugin("PremiumVanish") != null) {
-			if (Bukkit.getPluginManager().getPlugin("SuperVanish") != null) {
-				Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &e- &bSuperVanish &7detected (&av" + Bukkit.getPluginManager().getPlugin("SuperVanish").getDescription().getVersion() + "&7)"));
-			} else {
-				Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &e- &bPremiumVanish &7detected (&av" + Bukkit.getPluginManager().getPlugin("PremiumVanish").getDescription().getVersion() + "&7)"));
-			}
-			Bukkit.getConsoleSender().sendMessage(" ");
+			if (Bukkit.getPluginManager().getPlugin("SuperVanish") != null)
+				USPMessages.logCol("  &e- &bSuperVanish &7detected (&av" + Bukkit.getPluginManager().getPlugin("SuperVanish").getDescription().getVersion() + "&7)");
+			else
+				USPMessages.logCol("  &e- &bPremiumVanish &7detected (&av" + Bukkit.getPluginManager().getPlugin("PremiumVanish").getDescription().getVersion() + "&7)");
 		} else {
-			Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &e- &eSuperVanish &cor &ePremiumVanish &cnot detected, disabling &6Vanish &ccheck. &8(&bSuperVanish only&8)"));
-			Bukkit.getConsoleSender().sendMessage(" ");
-			USPConfig.get().set("SuperVanish.IgnoreVanished", Boolean.valueOf(false));
+			USPMessages.logCol("  &e- &eSuperVanish &cor &ePremiumVanish &cnot detected, disabling &6Vanish &ccheck. &8(&bSuperVanish only&8)");
+			USPConfig.get().set("SuperVanish.IgnoreVanished", false);
 			USPConfig.save();
 			USPConfig.reload();
 		}
+		USPMessages.log(" ");
 	}
 
 	private void checkUpdates() {
 		if(USPConfig.getBoolean(USPSetting.UPDATER_ENABLED) && USPConfig.getBoolean(USPSetting.UPDATER_MESSAGE_CONSOLE)) {
 			UpdateChecker.getLatestVersion(version -> {
-				Bukkit.getConsoleSender().sendMessage(" ");
+				USPMessages.log(" ");
 				if(isLatest(version)) {
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&e          uSleep &7update checker"));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- &7You are running the latest version."));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
+					USPMessages.logCol("&8|------------------------------------------>");
+					USPMessages.log(" ");
+					USPMessages.logCol("&e          uSleep &7update checker");
+					USPMessages.log(" ");
+					USPMessages.logCol("&b- &7You are running the latest version.");
+					USPMessages.log(" ");
+					USPMessages.logCol("&8|------------------------------------------>");
 				} else {
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&e          uSleep &7update checker"));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- &7A new version is available&8: &6v"+ version));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- &7Currently using&8: &cv"+getDescription().getVersion()));
-					Bukkit.getConsoleSender().sendMessage(" ");
-					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8|------------------------------------------>"));
+					USPMessages.logCol("&8|------------------------------------------>");
+					USPMessages.log(" ");
+					USPMessages.logCol("&e          uSleep &7update checker");
+					USPMessages.log(" ");
+					USPMessages.logCol("&b- &7A new version is available&8: &6v"+ version);
+					USPMessages.log(" ");
+					USPMessages.logCol("&b- &7Currently using&8: &cv"+getDescription().getVersion());
+					USPMessages.log(" ");
+					USPMessages.logCol("&8|------------------------------------------>");
 				}
-				Bukkit.getConsoleSender().sendMessage(" ");
+				USPMessages.log(" ");
 			});
 		}
 	}

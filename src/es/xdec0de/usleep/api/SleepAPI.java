ef931d592e2b0e26544b6669c84be0be06c69035
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-27 17:58:09 +0100

    Optimized SleepAPI (WIP) & USleepCMD

--- src/es/xdec0de/usleep/api/SleepAPI.java
+++ src/es/xdec0de/usleep/api/SleepAPI.java
@@ -2,103 +2,79 @@ package es.xdec0de.usleep.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+
 import com.earth2me.essentials.Essentials;
+
+import es.xdec0de.usleep.utils.SuperVanish;
 import es.xdec0de.usleep.utils.USPMessage;
 import es.xdec0de.usleep.utils.USPSetting;
-import es.xdec0de.usleep.utils.SuperVanish;
 import es.xdec0de.usleep.utils.files.USPConfig;
 import es.xdec0de.usleep.utils.files.USPMessages;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 
 public class SleepAPI {
-	
+
 	public static int NumSleep = 0;	
-	
+
 	public int getPlayerCount() {
 		List<Player> list = new ArrayList<Player>();
 		if(USPConfig.getBoolean(USPSetting.ESSENTIALS_IGNORE_AFK) && Bukkit.getPluginManager().getPlugin("Essentials") != null) {
 			Essentials ess = (Essentials)Bukkit.getServer().getPluginManager().getPlugin("Essentials");
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				if(!list.contains(p)) {
-					if(ess.getUser(p).isAfk()) {
-						list.add(p);
-					}
-				}
-	        }
+			for(Player p : Bukkit.getOnlinePlayers().stream().filter(on -> !list.contains(on)).collect(Collectors.toList()))
+				if(ess.getUser(p).isAfk())
+					list.add(p);
 		}
 		if(USPConfig.getBoolean(USPSetting.IGNORE_VANISHED)) {
 			if(Bukkit.getPluginManager().getPlugin("Essentials") != null) {
 				Essentials ess = (Essentials)Bukkit.getServer().getPluginManager().getPlugin("Essentials");
-				for(Player p : Bukkit.getOnlinePlayers()) {
-					if(!list.contains(p)) {
-						if(ess.getUser(p).isVanished()) {
-							list.add(p);
-						}
-					}
-		        }
+				for(Player p : Bukkit.getOnlinePlayers().stream().filter(on -> !list.contains(on)).collect(Collectors.toList()))
+					if(ess.getUser(p).isVanished())
+						list.add(p);
 			}
 			if(Bukkit.getPluginManager().getPlugin("SuperVanish") != null || Bukkit.getPluginManager().getPlugin("PremiumVanish") != null) {
-				for(Player p : Bukkit.getOnlinePlayers()) {
-					if(!list.contains(p)) {
-			            if(SuperVanish.isVanished(p)) {
-			            	list.add(p);
-			            }
-					}
-		        } 
+				for(Player p : Bukkit.getOnlinePlayers().stream().filter(on -> !list.contains(on)).collect(Collectors.toList()))
+					if(SuperVanish.isVanished(p))
+						list.add(p);
 			}
 		}
 		return Bukkit.getOnlinePlayers().size() - list.size();
 	}
 	
 	public void broadcastInstantNextDay(Player p) {
-		Bukkit.broadcastMessage(USPMessages.getMessage(USPMessage.INSTANT_OK).replaceAll("%player%", p.getName()));
-		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED)) {
-			for (Player on : Bukkit.getOnlinePlayers()) {
-                on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_NEXT_DAY_SOUND)), 1.0F, 1.0F); 
-			}
-		}
+		USPMessages.broadcast(USPMessage.INSTANT_OK, "%player%", p.getName());
+		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED))
+			Bukkit.getOnlinePlayers().forEach(on -> on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_NEXT_DAY_SOUND)), 1.0F, 1.0F));
 	}
 	
 	public void broadcastPercentNextDay() {
-		Bukkit.broadcastMessage(USPMessages.getMessage(USPMessage.PERCENT_NEXT_DAY));
-		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED)) {
-			for (Player on : Bukkit.getOnlinePlayers()) {
-                on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_NEXT_DAY_SOUND)), 1.0F, 1.0F); 
-			}
-		}
+		USPMessages.broadcast(USPMessage.PERCENT_NEXT_DAY);
+		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED))
+			Bukkit.getOnlinePlayers().forEach(on -> on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_NEXT_DAY_SOUND)), 1.0F, 1.0F));
 	}
 	
 	public void broadcastSleep() {
-		if(USPConfig.getBoolean(USPSetting.ACTIONBAR_ENABLED)) {
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(USPMessages.getMessage(USPMessage.PERCENT_OK).replaceAll("%required%", Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F))).replaceAll("%current%", Integer.toString(NumSleep))));
-			}
-		} else {
-			Bukkit.broadcastMessage(USPMessages.getMessage(USPMessage.PERCENT_OK).replaceAll("%required%", Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F))).replaceAll("%current%", Integer.toString(NumSleep)));
-		}
-		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED)) {
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				p.playSound(p.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_SOUND)), 1.0F, 1.0F); 
-			}
-		}
+		String required = Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F));
+		String current = Integer.toString(NumSleep);
+		if(USPConfig.getBoolean(USPSetting.ACTIONBAR_ENABLED))
+			USPMessages.broadcastActionbar(USPMessage.PERCENT_OK, "%required%", required, "%current%", current);
+		else
+			USPMessages.broadcast(USPMessage.PERCENT_OK, "%required%", required, "%current%", current);
+		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED))
+			Bukkit.getOnlinePlayers().forEach(on -> on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_SOUND)), 1.0F, 1.0F));
 	}
 	
 	public void broadcastWakeUp() {
-		if(USPConfig.getBoolean(USPSetting.ACTIONBAR_ENABLED)) {
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(USPMessages.getMessage(USPMessage.PERCENT_OK).replaceAll("%required%", Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F))).replaceAll("%current%", Integer.toString(NumSleep))));
-			}
-		} else {
-			Bukkit.broadcastMessage(USPMessages.getMessage(USPMessage.PERCENT_OK).replaceAll("%required%", Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F))).replaceAll("%current%", Integer.toString(NumSleep)));
-		}
-		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED)) {
-			for(Player p : Bukkit.getOnlinePlayers()) {
-				p.playSound(p.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_LEAVE_SOUND)), 1.0F, 1.0F); 
-			}
-		}
+		String required = Integer.toString(Math.round(getPlayerCount() * USPConfig.get().getInt("Events.PercentSleep.Percentage") / 100.0F));
+		String current = Integer.toString(NumSleep);
+		if(USPConfig.getBoolean(USPSetting.ACTIONBAR_ENABLED))
+			USPMessages.broadcastActionbar(USPMessage.PERCENT_OK, "%required%", required, "%current%", current);
+		else
+			USPMessages.broadcast(USPMessage.PERCENT_OK, "%required%", required, "%current%", current);
+		if(USPConfig.getBoolean(USPSetting.PERCENT_SLEEP_SOUNDS_ENABLED))
+			Bukkit.getOnlinePlayers().forEach(on -> on.playSound(on.getLocation(), Sound.valueOf(USPConfig.getString(USPSetting.PERCENT_SLEEP_LEAVE_SOUND)), 1.0F, 1.0F));
 	}
 }
--- src/es/xdec0de/usleep/cmds/USleepCMD.java
+++ src/es/xdec0de/usleep/cmds/USleepCMD.java
@@ -11,25 +11,21 @@ import es.xdec0de.usleep.utils.files.USPMessages;
 public class USleepCMD implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
-		if (args.length == 0) {
-			USPMessages.sendMessage(sndr, USPMessage.USLEEP_USAGE);
-		} else if (args.length == 1) {
+		if (args.length == 1) {
 			switch(args[0].toLowerCase()) {
 			case "reload": case "rl":
 				if(sndr.hasPermission(USPConfig.getString(USPSetting.RELOAD_PERM))) {
 					USPConfig.reload();
-		            USPMessages.reload();
-		            USPMessages.sendMessage(sndr, USPMessage.RELOAD);
-		        } else {
-		        	sndr.sendMessage(USPMessages.getMessage(USPMessage.NO_PERMS).replaceAll("%perm%", USPConfig.getString(USPSetting.RELOAD_PERM)));
-		        } 
+					USPMessages.reload();
+					USPMessages.sendMessage(sndr, USPMessage.RELOAD);
+				} else
+					USPMessages.sendMessage(sndr, USPMessage.NO_PERMS, "%perm%", USPConfig.getString(USPSetting.RELOAD_PERM));
 				break;
 			default:
 				USPMessages.sendMessage(sndr, USPMessage.USLEEP_USAGE);
 			}
-		} else {
+		} else
 			USPMessages.sendMessage(sndr, USPMessage.USLEEP_USAGE);
-		}  
 		return true;
 	}
 }

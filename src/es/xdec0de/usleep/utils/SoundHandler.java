9169ff5dc658be293262460d96bf3dbdc345b6de
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-02-02 00:02:45 +0100

    NotificationHandler renamed to SoundHandler & added broadcastSound for player lists

--- src/es/xdec0de/usleep/utils/SoundHandler.java
+++ src/es/xdec0de/usleep/utils/SoundHandler.java
@@ -1,12 +1,14 @@
 package es.xdec0de.usleep.utils;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 import es.xdec0de.usleep.utils.files.USPSetting;
 
-public class NotificationHandler {
+public class SoundHandler {
 
 	public static void playSound(Player player, Sound sound) {
 		if(sound != null)
@@ -19,6 +21,15 @@ public class NotificationHandler {
 			Sound sound = (Sound)EnumUtils.getEnum(Sound.class, soundStr);
 			if(sound != null)
 				Bukkit.getOnlinePlayers().forEach(on -> on.playSound(on.getLocation(), sound, 1.0F, 1.0F));
+		}
+	}
+
+	public static void broadcastSound(List<Player> players, USPSetting setting) {
+		String soundStr = setting.asString();
+		if(soundStr != null && !soundStr.isEmpty()) {
+			Sound sound = (Sound)EnumUtils.getEnum(Sound.class, soundStr);
+			if(sound != null)
+				players.forEach(on -> on.playSound(on.getLocation(), sound, 1.0F, 1.0F));
 		}
 	}
 }

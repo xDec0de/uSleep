00d61fd32e15cb1e3f68d24a55518c4bf2395e44
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-03-16 09:50:18 +0100

    Documented SleepGroup

--- src/es/xdec0de/usleep/api/SleepGroup.java
+++ src/es/xdec0de/usleep/api/SleepGroup.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 import es.xdec0de.usleep.api.events.NightSkipEvent;
@@ -15,6 +16,15 @@ import es.xdec0de.usleep.utils.files.USPMessage;
 import es.xdec0de.usleep.utils.files.USPSetting;
 import es.xdec0de.usleep.utils.files.USPWorlds;
 
+/**
+ * A class representing a group of
+ * worlds sharing their sleep count
+ * and other sleep-related attributes.
+ * 
+ * @author xDec0de_
+ *
+ * @since v2.0.0
+ */
 public class SleepGroup {
 
 	List<World> worlds = new ArrayList<World>();
@@ -31,6 +41,28 @@ public class SleepGroup {
 			this.percent = USPWorlds.getPercentRequired(id);
 	}
 
+	/**
+	 * Handles a <b>player</b> sleeping, essentially augmenting
+	 * {@link #getPlayersSleeping()} by one on percent sleep
+	 * and directly skipping the night on instant sleep.
+	 * This <b>DOES NOT</b> force <b>player</b> to enter a bed,
+	 * for that you can use {@link HumanEntity#sleep(org.bukkit.Location, boolean)}
+	 * which is recommended over this as this method is only
+	 * intended for sleep count handling while canceling uSleep's
+	 * events to override it's default behavior.
+	 * 
+	 * @param player the player sleeping, see {@link HumanEntity#isSleeping()},
+	 * uSleep doesn't check if the <b>player</b> is already sleeping as
+	 * this shouldn't happen in any case with the default plugin behavior.
+	 * @param forced whether to bypass permission check or not.
+	 * 
+	 * @return true if the player was allowed to sleep, false otherwise.
+	 * 
+	 * @since v2.0.0
+	 * 
+	 * @see SleepHandleEvent
+	 * 
+	 */
 	public boolean handleSleep(Player player, boolean forced) {
 		SleepMode mode = null;
 		if(USPSetting.INSTANT_SLEEP_ENABLED.asBoolean() && (forced || player.hasPermission(USPSetting.PERM_INSTANT_SLEEP.asString())))
@@ -59,6 +91,17 @@ public class SleepGroup {
 		return false;
 	}
 
+	/**
+	 * Handles a player waking up, essentially decreasing
+	 * {@link #getPlayersSleeping()} by one if it's > 0.
+	 * This <b>DOES NOT</b> force any player to leave a bed,
+	 * for that you can use {@link HumanEntity#wakeup(boolean)}
+	 * which is recommended over this as this method is only
+	 * intended for sleep count handling while canceling uSleep's
+	 * events to override it's default behavior.
+	 * 
+	 * @since v2.0.0
+	 */
 	public void handleWakeUp() {
 		if(sleeping > 0) {
 			sleeping--;
@@ -93,14 +136,41 @@ public class SleepGroup {
 		}
 	}
 
+	/**
+	 * Gets the required amount of players to skip a night.
+	 * 
+	 * @return the required amount of players to skip a night.
+	 * 
+	 * @since v2.0.0
+	 */
 	public int getRequiredPlayers() {
 		return Math.round((getPlayers().size() - getInactivePlayers().size()) * percent / 100.0F);
 	}
 
+	/**
+	 * Gets the list of afk or vanished players in {@link #getPlayers()}.
+	 * Depending on configuration uSleep can ignore either group
+	 * individually, so checking their respective setting is recommended.
+	 *
+	 * @return a new list of players containing the inactive players.
+	 * 
+	 * @see USleepAPI#getInactivePlayers(Collection, boolean, boolean)
+	 * @see USPSetting#PERCENT_SLEEP_IGNORE_AFK
+	 * @see USPSetting#PERCENT_SLEEP_IGNORE_VANISHED
+	 * 
+	 * @since v2.0.0
+	 */
 	public Collection<Player> getInactivePlayers() {
 		return USleepAPI.getInstance().getInactivePlayers(getPlayers(), USPSetting.PERCENT_SLEEP_IGNORE_AFK.asBoolean(), USPSetting.PERCENT_SLEEP_IGNORE_VANISHED.asBoolean());
 	}
 
+	/**
+	 * Gets the list of players currently present on this group.
+	 * 
+	 * @return the list of players on this group.
+	 * 
+	 * @since v2.0.0
+	 */
 	public List<Player> getPlayers() {
 		List<Player> players = new ArrayList<Player>();
 		for(World world : worlds)
@@ -108,22 +178,59 @@ public class SleepGroup {
 		return players;
 	}
 
+	/**
+	 * Gets the ID of the group specified in <b>worlds.yml</b>
+	 * 
+	 * @return the ID of the group.
+	 * 
+	 * @since v2.0.0
+	 */
 	public String getID() {
 		return id;
 	}
 
+	/**
+	 * Gets the list of worlds in this group.
+	 * 
+	 * @return the list of worlds in this group.
+	 * 
+	 * @since v2.0.0
+	 */
 	public List<World> getWorlds() {
 		return worlds;
 	}
 
+	/**
+	 * Checks if a world is present on this group.
+	 * 
+	 * @param world the world to check.
+	 * 
+	 * @return true if the world is on the group, false otherwise.
+	 * 
+	 * @since v2.0.0
+	 */
 	public boolean contains(World world) {
 		return worlds.contains(world);
 	}
 
+	/**
+	 * Gets the amount of players sleeping in this group.
+	 * 
+	 * @return the amount of players sleeping in this group.
+	 * 
+	 * @since v2.0.0
+	 */
 	public int getPlayersSleeping() {
 		return sleeping;
 	}
 
+	/**
+	 * Whether the group is skipping the night or not.
+	 * 
+	 * @return whether the group is skipping the night or not.
+	 * 
+	 * @since v2.0.0
+	 */
 	public boolean isNightSkipping() {
 		return isNightSkipping;
 	}

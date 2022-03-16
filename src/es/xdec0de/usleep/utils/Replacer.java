0689dc4744ebd7a9f4b4788d09b76128af43f201
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-03-16 08:16:22 +0100

    Spelling fix on Replacer's variables & documentation

--- src/es/xdec0de/usleep/utils/Replacer.java
+++ src/es/xdec0de/usleep/utils/Replacer.java
@@ -9,7 +9,7 @@ import java.util.List;
  * create a replacer variable and apply it to as many strings as you want to <b>avoid creating multiple instances of the same replacements</b>, also, 
  * make sure that the amount of strings added to the replacer are <b>even</b>, otherwise, an exception will be thrown!<br><br>
  * 
- * The default replacer stands for a replacer with the %prefix% and %error% replaces added to it.
+ * The default replacer stands for a replacer with the %prefix% and %error% replacements added to it.
  * 
  * @see Replacer#Replacer(String...)
  */
@@ -22,13 +22,13 @@ public class Replacer {
 	 * create a replacer variable and apply it to as many strings as you want to <b>avoid creating multiple instances of the same replacements</b>, also, 
 	 * make sure that the amount of strings added to the replacer are <b>even</b>, otherwise, an exception will be thrown!
 	 * 
-	 * @param replaces The strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
+	 * @param replacements The strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
 	 * 
 	 * @see #replaceAt(String)
 	 * @see #replaceAt(List)
 	 */
-	public Replacer(String... replaces) {
-		replaceList.addAll(Arrays.asList(replaces));
+	public Replacer(String... replacements) {
+		replaceList.addAll(Arrays.asList(replacements));
 		if(replaceList.size() % 2 != 0)
 			throw new IllegalArgumentException(replaceList.get(replaceList.size() - 1) + "does not have a replacer! Add one more element to the replacer.");
 	}
@@ -37,16 +37,16 @@ public class Replacer {
 	 * Adds new strings to an existing replacer, the amount of strings must also be even, note that existing replacements won't be ignored 
 	 * and the new replacer won't overwrite them.
 	 * 
-	 * @param replaces The new strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
+	 * @param replacements The new strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
 	 * 
 	 * @return The old replacer with the new replacements added to it.
 	 * 
-	 * @throws IllegalArgumentException if the amount of strings is not even, more technically, if replaces size % 2 is not equal to 0.
+	 * @throws IllegalArgumentException if the amount of strings is not even, more technically, if replacements size % 2 is not equal to 0.
 	 */
-	public Replacer add(String... replaces) {
-		if(replaces.length % 2 != 0)
-			throw new IllegalArgumentException(replaces[replaces.length -1] + "does not have a replacer! Add one more element to the replacer.");
-		replaceList.addAll(Arrays.asList(replaces));
+	public Replacer add(String... replacements) {
+		if(replacements.length % 2 != 0)
+			throw new IllegalArgumentException(replacements[replacements.length -1] + "does not have a replacer! Add one more element to the replacer.");
+		replaceList.addAll(Arrays.asList(replacements));
 		return this;
 	}
 

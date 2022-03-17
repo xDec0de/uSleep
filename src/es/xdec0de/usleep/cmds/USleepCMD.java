5521db2d4b4ed475ae1a98320d905eaa5733e9c2
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-03-17 22:26:59 +0100

    Added tab completion for /usleep

--- src/es/xdec0de/usleep/cmds/USleepCMD.java
+++ src/es/xdec0de/usleep/cmds/USleepCMD.java
@@ -1,8 +1,11 @@
 package es.xdec0de.usleep.cmds;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
 import es.xdec0de.usleep.utils.files.USPConfig;
 import es.xdec0de.usleep.utils.files.USPMessage;
@@ -10,25 +13,31 @@ import es.xdec0de.usleep.utils.files.USPMessages;
 import es.xdec0de.usleep.utils.files.USPSetting;
 import es.xdec0de.usleep.utils.files.USPWorlds;
 
-public class USleepCMD implements CommandExecutor {
+public class USleepCMD implements TabExecutor {
 
-	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length == 1) {
 			switch(args[0].toLowerCase()) {
 			case "reload": case "rl":
-				if(sndr.hasPermission(USPSetting.PERM_RELOAD.asString())) {
+				if(sender.hasPermission(USPSetting.PERM_RELOAD.asString())) {
 					USPConfig.setup(true);
 					USPMessages.setup(true);
 					USPWorlds.setup();
-					USPMessage.RELOAD.send(sndr);
+					USPMessage.RELOAD.send(sender);
 				} else
-					USPMessage.NO_PERMS.send(sndr, "%perm%", USPSetting.PERM_RELOAD.asString());
+					USPMessage.NO_PERMS.send(sender, "%perm%", USPSetting.PERM_RELOAD.asString());
 				break;
 			default:
-				USPMessage.USLEEP_USAGE.send(sndr);
+				USPMessage.USLEEP_USAGE.send(sender);
+				break;
 			}
 		} else
-			USPMessage.USLEEP_USAGE.send(sndr);
+			USPMessage.USLEEP_USAGE.send(sender);
 		return true;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+		return args.length == 1 && sender.hasPermission(USPSetting.PERM_RELOAD.asString()) ? Arrays.asList("reload") : Arrays.asList("");
 	}
 }

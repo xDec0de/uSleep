7d143c754d9fe09a5547f5366807d5eaba5c7f51
xDec0de_ <xdec0deyt@gmail.com>
2023-07-22 12:59:54 +0200

    Updated and cleaned /bedtp

--- src/es/xdec0de/usleep/cmds/BedTP.java
+++ src/es/xdec0de/usleep/cmds/BedTP.java
@@ -1,69 +1,44 @@
 package es.xdec0de.usleep.cmds;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import es.xdec0de.usleep.api.USleep;
 import es.xdec0de.usleep.api.events.BedTeleportTryEvent;
-import es.xdec0de.usleep.utils.files.USPMessage;
-import es.xdec0de.usleep.utils.files.USPSetting;
+import me.xdec0de.mcutils.general.commands.MCCommand;
 
-public class BedTP implements TabExecutor {
+public class BedTP extends MCCommand<USleep> {
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
-		if(sndr instanceof Player) {
-			Player p = (Player)sndr;
-			if(args.length == 0) {
-				if(p.hasPermission(USPSetting.PERM_BEDTP_SELF.asString())) {
-					Location bed = p.getBedSpawnLocation();
-					BedTeleportTryEvent bte = new BedTeleportTryEvent(p, p, bed);
-					Bukkit.getPluginManager().callEvent(bte);
-					if(!bte.isCancelled() && (bed = bte.getBedSpawnLocation()) != null) {
-						p.teleport(bed);
-						USPMessage.BEDTP_TELEPORT.send(p);
-					} else
-						USPMessage.BEDTP_ERROR.send(p);
-				} else
-					USPMessage.NO_PERMS.send(p, "%perm%", USPSetting.PERM_BEDTP_SELF.asString());
-			} else if(args.length == 1) {
-				if(sndr.hasPermission(USPSetting.PERM_BEDTP_OTHER.asString())) {
-					OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
-					Location bed = target != null ? target.getBedSpawnLocation() : null;
-					BedTeleportTryEvent bte = new BedTeleportTryEvent(p, target, bed);
-					Bukkit.getPluginManager().callEvent(bte);
-					if(!bte.isCancelled() && (bed = bte.getBedSpawnLocation()) != null) {
-						p.teleport(bed);
-						if(target != null && target.getUniqueId().equals(p.getUniqueId()))
-							USPMessage.BEDTP_TELEPORT.send(p);
-						else
-							USPMessage.BEDTP_TELEPORT_OTHER.send(p, "%player%", args[0]);
-					} else
-						USPMessage.BEDTP_ERROR.send(p);
-				} else
-					USPMessage.NO_PERMS.send(p, "%perm%", USPSetting.PERM_BEDTP_SELF.asString());
-			} else
-				USPMessage.BEDTP_USAGE.send(p);
-		} else
-			USPMessage.NO_CONSOLE.send(sndr);
-		return true;
+	public BedTP(USleep uSleep) {
+		super(uSleep, "bedtp");
+		setRestrictedSenderClass(ConsoleCommandSender.class);
 	}
 
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length == 1 && sender.hasPermission(USPSetting.PERM_BEDTP_OTHER.asString())) {
-			List<String> players = new ArrayList<String>();
-			Bukkit.getServer().getOnlinePlayers().forEach(on -> players.add(on.getName()));
-			return players;
-		}
-		return Arrays.asList("");
+	public boolean onCommand(CommandSender sndr, String[] args) {
+		Player sender = (Player)sndr;
+		OfflinePlayer target = asOfflinePlayer(0, args, sender);
+		final boolean isSelfTp = args.length == 0;
+		final String perm = getPlugin().getConfig().getString(isSelfTp ? "perms.bedTP.self" : "perms.bedTP.other");
+		if (!sender.hasPermission(perm))
+			return getPlugin().getMessages().send(sender, "noPerms", "%perm%", perm);
+		if (target == null)
+			return getPlugin().getMessages().send(sender, "cmds.bedTP.offline", "%target%", args[0]);
+		BedTeleportTryEvent bte = new BedTeleportTryEvent(sender, target, target.getBedSpawnLocation());
+		if (bte.getBedSpawnLocation() == null)
+			return getPlugin().getMessages().send(sender, "noBed");
+		sender.teleport(bte.getBedSpawnLocation());
+		return getPlugin().getMessages().send(sender, isSelfTp ? "cmds.bedTP.tp" : "cmds.bedTP.tpOther", "%target%", target);
+	}
+
+	@Override
+	public List<String> onTab(CommandSender sender, String[] args) {
+		if (args.length == 1 && sender.hasPermission(getPlugin().getConfig().getString("perms.bedTP.other")))
+			return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
+		return null;
 	}
 }

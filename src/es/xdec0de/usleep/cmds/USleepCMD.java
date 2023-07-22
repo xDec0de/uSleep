1cb4ef084140d8f2facb5a5a8da376c35148f36d
xDec0de_ <xdec0deyt@gmail.com>
2023-07-22 07:58:45 +0200

    Update /usleep to MCCommand

--- src/es/xdec0de/usleep/cmds/USleepCMD.java
+++ src/es/xdec0de/usleep/cmds/USleepCMD.java
@@ -3,42 +3,38 @@ package es.xdec0de.usleep.cmds;
 import java.util.Arrays;
 import java.util.List;
 
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
 
-import es.xdec0de.usleep.utils.files.USPConfig;
-import es.xdec0de.usleep.utils.files.USPMessage;
-import es.xdec0de.usleep.utils.files.USPMessages;
-import es.xdec0de.usleep.utils.files.USPSetting;
-import es.xdec0de.usleep.utils.files.USPWorlds;
+import es.xdec0de.usleep.api.USleep;
+import me.xdec0de.mcutils.general.commands.MCCommand;
 
-public class USleepCMD implements TabExecutor {
+public class USleepCMD extends MCCommand<USleep> {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (args.length == 1) {
-			switch(args[0].toLowerCase()) {
-			case "reload": case "rl":
-				if(sender.hasPermission(USPSetting.PERM_RELOAD.asString())) {
-					USPConfig.setup(true);
-					USPMessages.setup(true);
-					USPWorlds.setup();
-					USPMessage.RELOAD.send(sender);
-				} else
-					USPMessage.NO_PERMS.send(sender, "%perm%", USPSetting.PERM_RELOAD.asString());
-				break;
-			default:
-				USPMessage.USLEEP_USAGE.send(sender);
-				break;
-			}
-		} else
-			USPMessage.USLEEP_USAGE.send(sender);
-		return true;
+	public USleepCMD(USleep plugin) {
+		super(plugin, "usleep", "us");
 	}
 
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-		return args.length == 1 && sender.hasPermission(USPSetting.PERM_RELOAD.asString()) ? Arrays.asList("reload") : Arrays.asList("");
+	public boolean onCommand(CommandSender sender, String[] args) {
+		final String reloadPerm = getPlugin().getConfig().getString("Permissions.Reload");
+		if (!sender.hasPermission(reloadPerm))
+			return getPlugin().getMessages().send(sender, "NoPerms", "%perm%", reloadPerm);
+		if (args.length != 1)
+			return getPlugin().getMessages().send(sender, "Commands.usleep.Usage");
+		return switch (args[0].toLowerCase()) {
+		case "reload", "rl" -> reload(sender);
+		default -> getPlugin().getMessages().send(sender, "Commands.usleep.Usage");
+		};
+	}
+
+	private boolean reload(CommandSender sender) {
+		getPlugin().reload();
+		return getPlugin().getMessages().send(sender, "Commands.usleep.Reload");
+	}
+
+	@Override
+	public List<String> onTab(CommandSender sender, String[] args) {
+		final String reloadPerm = getPlugin().getConfig().getString("Permissions.Reload");
+		return args.length == 1 && sender.hasPermission(reloadPerm) ? Arrays.asList("reload") : Arrays.asList("");
 	}
 }
